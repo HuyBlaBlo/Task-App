0c1a41b700ef9f70329c2c664d7d2c86cbe4d4d1
Dao Gia Huy <huydgh04@gmail.com>
2026-07-02 14:19:01 +0700

    feat(backend): add GlobalExceptionHandler to catch IllegalArgument exception

---
+++ backend/src/main/java/com/huybla/tasks/controller/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.huybla.tasks.controller;
+
+import com.huybla.tasks.domain.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<ErrorResponse> handleExceptions(RuntimeException runtimeException, WebRequest webRequest){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                runtimeException.getMessage(),
+                webRequest.getDescription(false)
+        );
+        return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
+    }
+}
